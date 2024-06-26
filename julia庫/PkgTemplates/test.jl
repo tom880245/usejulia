91b71275f7b7305541ddef3d07fd3fe264cc016d
tom880245 <76754794+tom880245@users.noreply.github.com>
2024-06-26 22:16:10 +0800

    Update test.jl

--- julia庫/PkgTemplates/test.jl
+++ julia庫/PkgTemplates/test.jl
@@ -1,19 +1,5 @@
 using PkgTemplates
-Template(interactive=true)("MyPkg")
+#互動式 ("Packagename")
+Template(interactive=true)("Packagename")
 
-Template(;
-    user="tom880245",
-    dir="~/code",
-    authors="Acme Corp",
-    julia=v"1.1",
-    plugins=[
-        License(; name="MPL"),
-        Git(; manifest=true, ssh=true),
-        GitHubActions(; x86=true),
-        Codecov(),
-        Documenter{GitHubActions}(),
-        Develop(),
-    ],
-)
-
-
+#https://juliaci.github.io/PkgTemplates.jl/stable/
